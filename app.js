6df779e5c13f1f6c5fdafb90329363ea31603a4d
Martin Dobrev <martindobrev87@gmail.com>
2022-02-26 09:08:54 +0200

    App layout updatedy

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-var async = require('async');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
