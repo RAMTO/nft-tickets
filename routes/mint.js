a36a6ddba28e5e07b379a3b19d2fbc1b3057de39
Martin Dobrev <martindobrev87@gmail.com>
2022-10-10 20:58:52 +0300

    Debug

--- routes/mint.js
+++ routes/mint.js
@@ -40,16 +40,14 @@ async function mintNFT(ticketType, req) {
     process.env.CONTRACT_ADDRESS,
   );
 
-  console.log('contract', contract);
-
   const { bcadr: addressTo } = req.cookies;
-
-  console.log('addressTo', addressTo);
 
   try {
     const tx = await contract.mintNFT(addressTo, ticketType, {
       gasPrice: 600000000000,
     });
+
+    console.log('tx', tx);
 
     console.log('Minting...');
     await tx.wait();
